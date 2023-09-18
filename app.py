f4c75ea2aa6ef4b2d803e5c113a299b40636fa4a
Nick <nickgray@liverpool.ac.uk>
2023-09-18 15:49:34 +0100

    move to deploy spec

--- app.py
+++ app.py
@@ -152,5 +152,5 @@ def check():
   return "IM AWARE BACKEND"
     
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
-    # app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
